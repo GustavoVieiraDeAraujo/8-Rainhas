d79eccd2f43529d5d0a93eb8d3fca762f60c9607
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-01-09 14:08:29 -0300

    Teste 2

--- testa_oito_rainhas.py
+++ testa_oito_rainhas.py
@@ -3,6 +3,7 @@ Funções de teste das funções de implementação do caso da 8 rainhas.
 """
 
 from oito_rainhas import posiciona_rainhas_no_tabuleiro
+from oito_rainhas import pega_as_posicoes_das_rainhas
 
 def testa_montagem_tabuleiro_corretamente():
     '''
@@ -21,3 +22,22 @@ def testa_montagem_tabuleiro_corretamente():
                       [1, 0, 0, 0, 0, 0, 0, 0]]
 
     assert posiciona_rainhas_no_tabuleiro(entrada) == saida_esperada
+
+def testa_pega_as_posicoes_das_rainhas_corretamente():
+    '''
+    Esta função testa se a função pega_as_posicoes_das_rainhas
+    esta retornando as posições corretas da rainhas no tabuleiro.
+    '''
+
+    entrada = [[0, 0, 0, 0, 1, 0, 0, 0],
+               [0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 1, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0],
+               [0, 0, 1, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 1],
+               [0, 0, 0, 0, 0, 1, 0, 0],
+               [1, 0, 0, 0, 0, 0, 0, 0]]
+    saida_esperada = [[0, 4], [1, 1], [2, 3], [
+        3, 6], [4, 2], [5, 7], [6, 5], [7, 0]]
+
+    assert pega_as_posicoes_das_rainhas(entrada) == saida_esperada

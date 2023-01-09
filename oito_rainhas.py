db69e441fb107db404b2dbd4d0e6a86cbca8c50c
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-01-09 14:37:47 -0300

    Implementando função do Teste 3

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -26,3 +26,19 @@ def pega_as_posicoes_das_rainhas(tabuleiro):
             if tabuleiro[i][j] == 1:
                 lista_das_posicoes.append([i, j])
     return lista_das_posicoes
+
+def verifica_se_o_tabuleiro_tem_8_rainhas(tabuleiro):
+    '''
+    Esta função recebe uma matriz que representa um tabuleiro de xadrez.
+    A função percorre essa matriz contando a quantidade de rainhas presentes
+    no tabuleiro, as quais são representadas pelo algorismo 1, e retorna True
+    se tiver 8 rainhas e False se tiver mais ou menos que 8 rainhas.
+    '''
+    contador = 0
+    for linha in tabuleiro:
+        for posicao in linha:
+            if posicao == 1:
+                contador += 1
+    if contador == 8:
+        return True
+    return False

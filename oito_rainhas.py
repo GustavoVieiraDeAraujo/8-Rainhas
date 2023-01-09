3e195641985d6bdb9f47bb32e66a8322b78c84a3
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-01-09 14:20:09 -0300

    Implementando função do Teste 2

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -13,3 +13,16 @@ def posiciona_rainhas_no_tabuleiro(entrada):
     for linha in entrada.split():
         tabuleiro.append([int(elemento) for elemento in linha])
     return tabuleiro
+
+def pega_as_posicoes_das_rainhas(tabuleiro):
+    '''
+    Esta função recebe uma matriz que representa um tabuleiro de xadrez.
+    A função percorre essa matriz salvando a posição das rainhas, as quais
+    são representadas pelo algorismo 1, e retorna uma lista com as posições.
+    '''
+    lista_das_posicoes = []
+    for i in range(8):
+        for j in range(8):
+            if tabuleiro[i][j] == 1:
+                lista_das_posicoes.append([i, j])
+    return lista_das_posicoes

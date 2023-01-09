9e25f911387b0d2f5a1834b4530217bb601525e5
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-01-09 14:06:05 -0300

    Implementando função do Teste 1

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -1,3 +1,15 @@
 """
 Funções de Implementação do caso das 8 rainhas.
 """
+
+def posiciona_rainhas_no_tabuleiro(entrada):
+    '''
+    Esta função recebe uma entrada ( string com uma sequencia de zeros
+    e uns separados a cada 8 algorismo por um espaço ).A patir dessa
+    entrada a função monta uma matriz que representa essa entradae retorna
+    ela.
+    '''
+    tabuleiro = []
+    for linha in entrada.split():
+        tabuleiro.append([int(elemento) for elemento in linha])
+    return tabuleiro
